c2cd7652364becb548fc20f59ec3ce810715746c
Johnny De Luna <johndelunaaa@gmail.com>
2024-11-21 15:16:49 -0600

    updated js file

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -2,10 +2,19 @@ console.log('****** Loops Practice *******');
 
 
 // 1. 'for' loop
+
 console.log('---- 1. For loops ----');
 // Example: a for loop to console.log numbers from 0 to 3
 console.log('count from 0 to 3');
+
+
+
+
+
 // start i at 0, while i is < 4 do code between {}, afterwards add 1 to i (i++)
+
+
+
 for (let i=0; i<4; i++) {  
   console.log(i);
 }
@@ -14,18 +23,34 @@ for (let i=0; i<4; i++) {
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
 
+for (let i=0; i<6 ; i++) {
+  console.log(i);
+}
+
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+
+for (let index=3; index<6 ; index++) {
+  console.log(index);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
+for (let index=2; index<11 ; index++) {
+  if (index % 2 === 0) {
+    console.log(index);
+  }
+}
+
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
-
+for (let index=5; index>=0 ; index--) {
+  console.log(index);
+}
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
